dd67c413154687325674008d18c8e5138bd47760
imhidayatchitrali <hidayat.rehman64@gmail.com>
2026-01-12 15:58:52 +0500

    fixes

--- scripts/addUserRoles.js
+++ scripts/addUserRoles.js
@@ -3,11 +3,11 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const config = {
-  user: "postgres",
-  host: "localhost",
-  database: "autowise",
-  password: "admin",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   connectionTimeoutMillis: 10000,
   idleTimeoutMillis: 10000
 };
--- scripts/createAdminUser.js
+++ scripts/createAdminUser.js
@@ -4,11 +4,11 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const config = {
-  user: "postgres",
-  host: "localhost",
-  database: "autowise",
-  password: "admin",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   connectionTimeoutMillis: 10000,
   idleTimeoutMillis: 10000
 };
